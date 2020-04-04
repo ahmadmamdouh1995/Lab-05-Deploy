6c51464730762b876944d516b441ed4a96360102
ahmadmamdouh <mamdouh486@gmail.com>
2020-04-05 00:12:45 +0300

    first thing

---
+++ server.js
@@ -0,0 +1,7 @@
+'use strict';
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static('./public'));
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
